ff32575835ca77a473af9b3d4feccca4fa7ce044
Admin <arseniyserzhan@icloud.com>
2025-11-04 16:52:11 +0300

    UPDATE: write logic of main app router

---
+++ js/app.js
@@ -0,0 +1,18 @@
+import UsersView from './views/users.js';
+import TodosView from './views/todos.js';
+import PostsView from './views/posts.js';
+import CommentsView from './views/comments.js';
+import { Router } from './router.js';
+
+const root = document.getElementById('app');
+
+const routes = {
+  'users': UsersView,
+  'users#todos': TodosView,
+  'users#posts': PostsView,
+  'users#posts#comments': CommentsView
+};
+
+const router = new Router(root, routes);
+
+if (!location.hash) location.hash = '#users';
